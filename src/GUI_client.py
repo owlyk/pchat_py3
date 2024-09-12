24451fd33a4c0dc4f940ce7ba48c98ee2bc5eca5
Product Migrator <genbee@example.com>
2024-09-12 15:13:33 +0000

    Migration result task_id cc5df479-cf54-47b5-8b31-5f5b4bdace8a

---
+++ src/GUI_client.py
@@ -0,0 +1,37 @@
+import socket
+import threading
+from tkinter import *
+
+client = socket.socket()
+client.connect(('localhost', 63453))
+root = Tk()
+
+def send():
+    data_send = get_message.get()
+    if data_send != "":
+        data_send = str(data_send)
+        client.send(data_send.encode())
+        lbl = Label(root, text=data_send, bg="red", fg="white")
+        get_message.delete(0, END)
+        lbl.pack(fill=X, side=TOP)
+
+def recv():
+    while True:
+        data_recv = client.recv(1024).decode()
+        if data_recv != "":
+            lbl = Label(root, text=data_recv, bg="blue", fg="white")
+            lbl.pack(fill=X, side=TOP)
+
+get_message = Entry(root)
+send_message = Button(root, text="Send", command=send)
+
+send_message.pack(fill=X, side=BOTTOM)
+get_message.pack(fill=X, side=BOTTOM)
+
+threading.Thread(target=recv).start()
+
+root.title("Chat Client")
+root.geometry("500x700")
+root.resizable(width=False, height=False)
+
+root.mainloop()
